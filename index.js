2dbe2e1b7509f0149dca147c32564096854fe699
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-05-31 17:14:08 -0400

    Set up the OpenAI key to handle requests and responses

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import bodyParser from 'body-parser';
 import 'dotenv/config';
-import express from 'express';
+import express, { request } from 'express';
 import OpenAI from 'openai';
 
 
@@ -13,6 +13,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const openai = new OpenAI({
     apiKey: process.env.API_KEY
+})
+
+app.post('/converse', async(request, response) =>{
+    const message = request.body.message;
+    
+    const chatCompletion = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [{role: 'user', content: 'message'}]
+    })
+
+    response.send(chatCompletion.choices[0].message)
+    console.log(chatCompletion.choices[0].message)
 })
 
 app.listen(process.env.PORT, (error) =>{
